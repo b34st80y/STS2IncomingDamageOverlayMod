c08d6de4a8eb044b9ae3d7d19364acde3bbea4da
b34st80y <mailadamg@gmail.com>
2026-05-23 11:31:03 -0400

    added after osty

--- STS2IncomingDamageOverlayMod/Source/Domain/PlayerVitals.cs
+++ STS2IncomingDamageOverlayMod/Source/Domain/PlayerVitals.cs
@@ -5,5 +5,6 @@ namespace STS2IncomingDamageOverlayMod.Domain;
 internal readonly record struct PlayerVitals(
     int Block,
     int CurrentHp,
+    int OstyHp,
     bool HasDefensivePotion,
     Color CharacterColor);
